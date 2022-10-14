c190894daeb8c6722c6124897297979fa8272441
dj2237771 <dj2237771@gmail.com>
2022-10-14 21:00:47 +0100

    Figma project completed
    Figma project completed

---
+++ Lesson8/java.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+
+console.error("this is error");
+
+console.warn("this is warniog");
+
+let firstName = prompt("what is your name?");
+console.log(firstName);
+
+let myAge = prompt("what is your age?");
+
+console.log("my name is " + firstName + " and my age is " + myAge);
