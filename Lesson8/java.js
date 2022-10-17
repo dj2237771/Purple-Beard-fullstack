51d9d98ec2d6d30449c4903ab0e0fe720c6f22a8
dj2237771 <dj2237771@gmail.com>
2022-10-17 12:44:48 +0100

    lesson 8 h/w
    lesson 8 h/w

--- Lesson8/java.js
+++ Lesson8/java.js
@@ -3,10 +3,42 @@ console.log("hello world");
 console.error("this is error");
 
 console.warn("this is warniog");
-
+// exercise 1 age logger
 let firstName = prompt("what is your name?");
 console.log(firstName);
 
 let myAge = prompt("what is your age?");
 
 console.log("my name is " + firstName + " and my age is " + myAge);
+
+// exercise 2 so many rabits
+
+let numberOfRabbits = 2;
+
+numberOfRabbits = numberOfRabbits * 2;
+
+console.log(numberOfRabbits);
+
+// exercise 3 so many rabits
+
+let bodmasExample = 3;
+bodmasExample = (bodmasExample + 5) / 6;
+console.log(bodmasExample);
+
+// exercise 4 random number generator
+let randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log(randomNumber);
+
+// exercise 5
+
+let firstName2 = "John";
+let surname = "Smith";
+
+console.log(`my name is ${firstName2} and my surname is ${surname}`);
+console.log(`welcome ${firstName2} ${surname} how can i help you `);
+
+// exercise 6
+
+let myCountry = "india";
+console.log(myCountry.length);
+console.log(myCountry.toUpperCase());
