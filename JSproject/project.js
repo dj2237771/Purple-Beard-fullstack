acb52d6deb6f951b36f3627370e3fca0f071a274
dj2237771 <dj2237771@gmail.com>
2022-11-03 20:11:21 +0000

    finshed work

--- JSproject/project.js
+++ JSproject/project.js
@@ -2,6 +2,19 @@ const loginButton = document.getElementById("loginSubmit");
 const loginBox = document.getElementById("loginBox");
 const creatLoginForm = document.getElementById("createAccount");
 const creatProductForm = document.getElementById("creatProduct");
+const sinupPassword = document.getElementById("signupPassword");
+const strengthBar = document.getElementById("passwordStrenght");
+const addProduct = document.getElementById("creatProduct");
+const newProduct = document.getElementById("saleProducts");
+const productAllPrice = document.getElementById("allProPrice");
+const numberOfAllProduct = document.getElementById("numberOfPro");
+
+let strongPassword = new RegExp(
+  "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})"
+);
+let mediumPassword = new RegExp(
+  "((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))"
+);
 
 function formMessage(fromElement, type, message) {
   const messageBox = fromElement.querySelector(".formMessage");
@@ -11,10 +24,17 @@ function formMessage(fromElement, type, message) {
 }
 
 function loginForm(e) {
+  let gotenUsername = localStorage.getItem("username");
+  let gotenPassword = localStorage.getItem("password");
+
   e.preventDefault();
   console.log(loginBox.elements[0].value);
   console.log("its working");
-  if (loginBox.elements[0].value == "" && loginBox.elements[1].value == "") {
+  if (
+    (loginBox.elements[0].value == "" && loginBox.elements[1].value == "") ||
+    (loginBox.elements[0].value == gotenUsername &&
+      loginBox.elements[1].value == gotenPassword)
+  ) {
     creatProductForm.classList.remove("formHidden");
     loginBox.classList.add("formHidden");
   } else {
@@ -36,6 +56,24 @@ function clearInputError(inputElement) {
   inputElement.parentElement.querySelector(
     ".formInput-error-message"
   ).textContent = "";
+}
+function checkpassword(password) {
+  let strength = 0;
+  if (password.match(/[a0zA-Z0-9][a0zA-Z0-9]+/)) {
+    strength += 1;
+  }
+  if (password.match(/[~<>?]+/)) {
+    strength += 1;
+  }
+  if (password.match(/[¬!"£$%^&*()]+/)) {
+    strength += 1;
+  }
+  if (password.length > 5) {
+    strength += 1;
+  }
+  console.log(strength);
+
+  return (strengthBar.value = strength * 2);
 }
 
 document.querySelector("#creatAcount").addEventListener("click", (e) => {
@@ -71,4 +109,53 @@ document.querySelectorAll(".formInput").forEach((inputElement) => {
   inputElement.addEventListener("input", (e) => {
     clearInputError(inputElement);
   });
+});
+
+sinupPassword.addEventListener("keyup", function (e) {
+  e.preventDefault();
+  checkpassword(sinupPassword.value);
+});
+
+creatLoginForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(creatLoginForm.length);
+  for (i = 0; i < creatLoginForm.length - 1; i++) {
+    if (creatLoginForm.elements[i].value == "") {
+      inputError(creatLoginForm.elements[i], "Must be filled");
+    }
+    if (creatLoginForm.elements[2].value != creatLoginForm.elements[3].value) {
+      inputError(creatLoginForm.elements[2], "Password dose not match");
+    } else {
+      localStorage.setItem("username", creatLoginForm.elements[0].value);
+      localStorage.setItem("password", creatLoginForm.elements[2].value);
+      loginBox.classList.remove("formHidden");
+      creatLoginForm.classList.add("formHidden");
+    }
+  }
+});
+
+addProduct.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let numberOfProduct = 0;
+
+  numberOfProduct += 1;
+  console.log(addProduct.length);
+  for (i = 0; i < addProduct.length - 1; i++) {
+    let allProductPrice = 0;
+
+    if (addProduct.elements[i].value == "") {
+      inputError(addProduct.elements[i], "Must be filled");
+    }
+    if (addProduct.elements[5]) {
+      allProductPrice += addProduct.elements[8].value;
+    }
+    {
+      productAllPrice.textContent = allProductPrice;
+      numberOfAllProduct.textContent = numberOfProduct;
+      let para = document.createElement("p");
+      data = addProduct.elements[i].value;
+      para.innerText = addProduct.elements[i].value;
+      newProduct.appendChild(para);
+    }
+  }
 });
